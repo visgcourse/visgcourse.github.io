381760b9dabf50031ce8795710ae4aa32c8567f6
Su Tongkui <121180088@smail.nju.edu.cn>
2016-10-18 17:49:55 +0800

    Add files via upload

--- contents/c/C2_A1.c
+++ contents/c/C2_A1.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
+// read more and write more, you'll be the god/godness of the code
+//
+
+#include "stdafx.h"
 
 int main()
 {
-	double x;
-	scanf("%lf",&x);
-	double y;
-	if(x<1)
-		y=x;
-	else if(x<10)
-		y=2*x-1;
-	else
-		y=3*x-11;
-	printf("%f",y);
+	int x, y;
+	printf("*****input an integer:\nx = ");
+	scanf("%d", &x);
 
+	if (x < 1)
+		y = x;
+	else if (1 <= x && x < 10)
+		y = 2 * x - 1;
+	else
+		y = 3 * x - 1;
+
+	printf("y = %d", y);
 	return 0;
 }
