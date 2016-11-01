8e648216440da7e55caafb997edf8f4a4f495c27
Su Tongkui <121180088@smail.nju.edu.cn>
2016-11-01 17:23:55 +0800

    Add files via upload

---
+++ contents/c/C4_A1.c
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+//两种方法差别见函数gcd内循环体
+#define Euler
+
+int gcd(int ,int);
+long long lcm(int ,int);
+
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("最小公约数%d\n",gcd(a,b));
+	printf("最大公倍数%lld\n",lcm(a,b));
+	system("pause");
+	return 0;
+}
+int gcd(int x, int y)
+{
+	int temp;
+	if(x<y)
+	{
+		temp=y;
+		y=x;
+		x=temp;
+	}
+	while(y!=0)
+	{
+#ifdef Euler
+		temp=x%y;//辗转相除法(欧几里德算法)
+#else
+		temp=abs(x-y);//更相减损法
+#endif
+		x=y;
+		y=temp;
+	}
+	return x;
+}
+long long lcm(int x, int y)
+{
+	long long c=(long long)x*y;
+	c/=gcd(x,y);
+	return c;
+}
---
+++ contents/c/C4_A2.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+//题目本意不是用数组来解决的
+
+int main()
+{
+	char c,c_before=' ';
+	int lower=0,upper=0,words=0;
+	while((c=getchar())!='\n')
+	{
+		if(c_before==' '&&c!=' ')
+			++words;
+		
+		if(c>='a'&&c<='z')
+			++lower;
+		else if(c>='A'&&c<='Z')
+			++upper;
+		
+		c_before=c;
+	}
+	printf("小写字母数量%d  大写字母数量%d  单词数量%d\n",lower,upper,words);
+	system("pause");
+	return 0;
+}
